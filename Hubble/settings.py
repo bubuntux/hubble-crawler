34aed2fc559dceb605b63602a5c17d76c3fb37e9
Julio Gutierrez <lcc.julio@gmail.com>
2015-03-20 10:39:13 -0600

    Update settings.py

--- Hubble/settings.py
+++ Hubble/settings.py
@@ -6,4 +6,4 @@ SPIDER_MODULES = ['Hubble.spiders']
 NEWSPIDER_MODULE = 'Hubble.spiders'
 
 ITEM_PIPELINES = {'Hubble.pipelines.HubblePipeline': 1}
-IMAGES_STORE = 'CHANGE_ME'  # TODO use ${HOME}/Pictures/Hubble
+IMAGES_STORE = 'images'
